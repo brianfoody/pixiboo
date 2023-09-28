d6c13a7ca633ac4bfbb24d51ad8e66a04beced7b
Brian Foody <brianfoody@gmail.com>
2023-09-28 14:27:30 +0800

    Fix next.js ssr

--- components/InteractiveCanvas.tsx
+++ components/InteractiveCanvas.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Stage, Layer, Circle, Rect } from "react-konva";
 import { useSpring, animated } from "@react-spring/konva";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import InteractiveCanvas from "../components/InteractiveCanvas";
+import dynamic from "next/dynamic";
+
+const Canvas = dynamic(() => import("../components/InteractiveCanvas"), {
+  ssr: false,
+});
 
 export default function Home() {
-  return <InteractiveCanvas />;
+  return <Canvas />;
 }
